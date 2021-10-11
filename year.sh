429a54c317147e5af0c4d42c91b078e6cd3869f2
Sakshi Baburao Jogdand <2017bcs524@sggs.ac.in>
2021-10-11 19:41:28 +0530

    My second Shell script

---
+++ year.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Enter a year = "
+read year
+
+if [ $(( year%400 )) -eq 0 ] || [ $(( year%100 )) -ne 0 ] && [ $(( year%4 )) -eq 0 ]
+then 
+echo " $year is Leap Year "
+else
+echo " $year is not a Leap Year "
+fi
+
